f088908060baee1df0a556eeaaf7cb2f29f0f874
patricia.dumitru <patricia.dumitru@auto1.com>
2020-02-27 18:44:23 +0100

    first homework

--- src/app/service/BusinessReportService.java
+++ src/app/service/BusinessReportService.java
@@ -1,28 +1,33 @@
 package app.service;
-
+​
 import app.dtos.BusinessReportDTO;
 import app.dtos.CarDTO;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+​
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
+/*
+Problem 3: Out of memory issue. The issue appeared because of a huge list of reports that was keep being created,
+ in a condition that did not had any end. I chose to replace the creation of new BusinessReportDTO with method
+ createReport as it seems to have a fixed rate of only 500 reports.
+ */
 @Component
 public class BusinessReportService {
     private List<BusinessReportDTO> reports;
     public BusinessReportService(){
         reports=new ArrayList<>();
     }
-
+​
     public BusinessReportDTO getLastReport() {
         if(reports.isEmpty()){
-            return new BusinessReportDTO();
+            createReport();
         }
         return reports.get(reports.size()-1);
     }
-
+​
     @Scheduled(fixedRate = 500)
     public void createReport(){
         int amountOfCars= getNumberBetween(5000000,9000000);
@@ -33,7 +38,7 @@ public class BusinessReportService {
                 });
         reports.add(reportDTO);
     }
-
+​
     private CarDTO createCar() {
         int purchasePrice=getNumberBetween(10000, 100000);
         int priceSold=getNumberBetween(purchasePrice, 110000);
@@ -44,13 +49,13 @@ public class BusinessReportService {
                 .vin("V"+vin+"S")
                 .build();
     }
-
+​
     private int getNumberBetween(int min,int max) {
         Random random = new Random();
         int low = 5000;
         int high = 100000;
         return random.nextInt(high-low) + low;
     }
-
-
+​
+        ​
 }
